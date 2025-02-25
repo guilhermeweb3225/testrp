4f0b26e8088bd54ce1691b6592cf33c0f551178f
Guilherme P.Oliveira <guilhermeponciano3225@gmail.com>
2025-02-25 16:14:30 -0300

    prC

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/testrp" element={<Home/>}/>
+        <Route path="/" element={<Home/>}/>
         <Route path="/Serviços" element={<Serviços/>}/>
         <Route path="/Sobre" element={<Sobre/>}/>
         <Route path="/Contato" element={<Contato/>}/>
